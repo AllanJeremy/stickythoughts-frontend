7423b9cd1ae8e11d8028fae4225387d178cd0398
AllanJeremy <aj.dev254@gmail.com>
2021-04-27 15:33:48 +0300

    [Enhancement] Updated site description in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,17 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - audio-journal-frontend',
-    title: 'audio-journal-frontend',
+    titleTemplate: '%s - Audio Journal',
+    title: 'Record your thoughts - Audio Journal',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Record your thoughts and never forget a single thing with Audio Journal',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -53,7 +58,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
