e7bb2a8c74b801617fb805a448a2b26b1cf4dbe6
Allan Jeremy <aj.dev254@gmail.com>
2021-05-15 12:29:46 +0300

    [Enhancement] Added handling of duration even when the value provided for seconds is not a number Also handled division by zero errors that may have arised

--- mixins/FormatMixin.js
+++ mixins/FormatMixin.js
@@ -3,9 +3,11 @@ import _ from 'lodash'
 export default {
   filters: {
     formatTimer(durationInSeconds) {
-      const seconds = parseInt(durationInSeconds) % 60
-      const min = Math.floor(seconds / 60)
-      const hours = Math.floor(seconds / 3600)
+      durationInSeconds = parseInt(durationInSeconds)
+
+      const seconds = isNaN(durationInSeconds) ? 0 : durationInSeconds % 60
+      const min = seconds === 0 ? 0 : Math.floor(seconds / 60)
+      const hours = seconds === 0 ? 0 : Math.floor(seconds / 3600)
 
       let formattedTime = `${_.padStart(min, 2, '0')}:${_.padStart(
         seconds,
