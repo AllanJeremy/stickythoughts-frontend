6a7e84d6405deafd1bb3d9a88501424dba37d84d
Allan Jeremy <aj.dev254@gmail.com>
2021-05-24 21:54:15 +0300

    [Enhancement] Login and user creation on login now working correctly TODO: Add onboarding process

---
+++ pages/auth/login.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="mh-full d-flex flex-column align-center justify-center">
+    <img
+      src="/logo-plain.png"
+      height="256"
+      alt="StickyThoughts - Remember everything. Logo."
+    />
+
+    <h2 class="secondary--text">Welcome onboard</h2>
+    <p class="font-weight-light text-center">
+      You are one step away from preserving the thoughts that matter the most to
+      you.
+    </p>
+
+    <div ref="firebaseuiAuthContainer"></div>
+  </section>
+</template>
+
+<script>
+import { auth, firebase } from '@/helpers/firebase'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
+import { NavMixin } from '@/mixins'
+import { userApi } from '@/apiRequests'
+
+export default {
+  mixins: [NavMixin],
+  data() {
+    return {
+      // TODO: Add implementation
+    }
+  },
+  mounted() {
+    this.initializeFirebaseUi()
+  },
+  methods: {
+    initializeFirebaseUi() {
+      console.log()
+      // Initialize the FirebaseUI Widget using Firebase.
+      const ui = new firebaseui.auth.AuthUI(auth)
+
+      ui.start(this.$refs.firebaseuiAuthContainer, {
+        signInOptions: [
+          // List of OAuth providers supported.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        signInFlow: 'popup',
+        signInSuccessUrl: '/journal/record',
+        callbacks: {
+          signInSuccessWithAuthResult({ user }, redirectUrl) {
+            console.log('Redirect url:', redirectUrl)
+
+            const newUserData = {
+              uid: user.uid,
+              name: user.displayName,
+              email: user.email,
+            }
+
+            // User successfully signed in.
+            return userApi.createUser(newUserData).then(() => {
+              this.goTo('/journal/record')
+            })
+          },
+          uiShown() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none'
+          },
+        },
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.mh-full {
+  min-height: calc(100vh - 4rem);
+}
+</style>
