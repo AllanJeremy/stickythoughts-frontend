6eb298e199ad3c78832427635bc11ac9691eae78
Allan Jeremy <aj.dev254@gmail.com>
2021-05-25 21:19:47 +0300

    [Feature] Added onboarding layout

---
+++ layouts/onboarding.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <div id="onboarding-bg"></div>
+      <v-container class="fill-height d-flex flex-column justify-center">
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+#onboarding-bg {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: url('/empty-notepad-minimalist.jpg');
+  background-size: cover;
+  background-position: center;
+  filter: blur(3px);
+}
+</style>
