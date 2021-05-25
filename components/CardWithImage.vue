e11331157ed77085a0e1a240f99d99a7ac9c241d
Allan Jeremy <aj.dev254@gmail.com>
2021-05-25 13:06:28 +0300

    [Feature] Added card with image and empty container components

---
+++ components/CardWithImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mb-6" :width="width">
+    <v-img
+      class="white--text align-center"
+      :alt="alt"
+      :height="height"
+      gradient="to top right, rgba(0,0,0,.3), rgba(0,0,0,.7)"
+      :src="imageUrl"
+    >
+      <v-card-title><slot name="title"></slot></v-card-title>
+      <v-card-subtitle><slot></slot></v-card-subtitle>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardWithImage',
+  props: {
+    alt: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '6.5rem',
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/EmptyContainer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container class="h-100 d-flex flex-column justify-center">
+    <section class="text-center header-content">
+      <slot name="header"></slot>
+    </section>
+
+    <section v-if="imageUrl.length" class="d-flex mx-auto mb-4">
+      <img :src="imageUrl" :alt="alt" :style="`opacity: ${opacity}`" />
+    </section>
+
+    <section class="text-center body-content"><slot></slot></section>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'EmptyContainer',
+  props: {
+    imageUrl: {
+      type: String,
+      default: '/no-data.svg', // Default svg we use for missing data
+    },
+    opacity: {
+      type: String,
+      default: '1.0',
+    },
+    alt: {
+      type: String,
+      default: 'NewPine image',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.body-content,
+.header-content h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  line-height: 1.25;
+  margin-bottom: 8px !important;
+}
+
+img {
+  width: 100%;
+  max-height: 15rem;
+  max-width: 20rem;
+  align-self: center;
+}
+
+.h-100 {
+  height: 100%;
+}
+</style>
