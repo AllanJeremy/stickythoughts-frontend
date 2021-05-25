c2f094f0ec22ccd806179c8e124ca64dc5802e9b
Allan Jeremy <aj.dev254@gmail.com>
2021-05-25 21:20:57 +0300

    [Enhancement] Added upload and auth mixin to mixins aggregator

--- mixins/index.js
+++ mixins/index.js
@@ -1,5 +1,11 @@
+/* 
+    This file is simply meant to be an aggregator for all mixins
+    This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
+*/
 import AudioPlayerMixin from './AudioPlayerMixin'
 import FormatMixin from './FormatMixin'
+import NavMixin from './NavMixin'
+import UploadMixin from './UploadMixin'
 
 // * EXPORTS
-export { AudioPlayerMixin, FormatMixin }
+export { AudioPlayerMixin, FormatMixin, NavMixin, UploadMixin }
