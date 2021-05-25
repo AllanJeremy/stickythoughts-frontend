5809addc133d3c6c37778ad72d76a2caddbf38dc
Allan Jeremy <aj.dev254@gmail.com>
2021-05-25 21:22:43 +0300

    [Enhancement] Card with image subtitle text defaults to left alignment and a white color Makes sense since it is text on an overlay to a background image

--- components/CardWithImage.vue
+++ components/CardWithImage.vue
@@ -1,14 +1,16 @@
 <template>
   <v-card class="mb-6" :width="width">
     <v-img
-      class="white--text align-center"
+      class="white--text"
       :alt="alt"
       :height="height"
       gradient="to top right, rgba(0,0,0,.3), rgba(0,0,0,.7)"
       :src="imageUrl"
     >
       <v-card-title><slot name="title"></slot></v-card-title>
-      <v-card-subtitle><slot></slot></v-card-subtitle>
+      <v-card-subtitle class="white--text text-left"
+        ><slot></slot
+      ></v-card-subtitle>
     </v-img>
   </v-card>
 </template>
