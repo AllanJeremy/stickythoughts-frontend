63e6a92fb026682ca8f32039e43bf89f235a3c48
Allan Jeremy <aj.dev254@gmail.com>
2021-05-26 08:00:32 +0300

    [Refactoring] Moved categories logic to categories form This will allow us to use this logic in different pages
    The component accepts an on-updated prop which expects a function that receives the latest categories as its argument
    
    There is also a prop to customize the title to be displayed incase that needs to be different on different pages. Defaults to "Let's get organized"

---
+++ components/forms/CategoriesForm.vue
@@ -0,0 +1,242 @@
+<template>
+  <div>
+    <h2 class="mb-2">{{ title }}</h2>
+    <p v-if="categories.length < 2" class="font-weight-light">
+      Please ensure you have at least 2 categories
+    </p>
+
+    <div class="mb-8 d-flex align-center">
+      <v-text-field
+        v-model="categoryToAdd"
+        solo
+        :label="
+          categoryLimitReached ? 'Category limit reached' : 'Category name'
+        "
+        :flat="categoryLimitReached"
+        :disabled="categoryLimitReached"
+        :background-color="categoryLimitReached ? '#DDD' : ''"
+        clearable
+        :hint="
+          categoryLimitReached
+            ? `Maximum of ${MAX_CATEGORIES} categories reached`
+            : ''
+        "
+        :persistent-hint="categoryLimitReached"
+        hide-details
+      ></v-text-field>
+
+      <v-btn
+        v-if="!categoryLimitReached"
+        class="ml-4"
+        color="teal"
+        large
+        outlined
+        :disabled="!canAddCategory || categoryLimitReached"
+        @click="addCategory"
+        >Add</v-btn
+      >
+    </div>
+
+    <section class="text-left mb-4">
+      <div></div>
+      <!-- Actual categories -->
+      <v-fade-transition>
+        <div v-if="categories.length" class="my-5">
+          <h3 class="secondary--text mb-2">
+            My categories
+            <small class="font-weight-light">(8 max)</small>
+          </h3>
+
+          <v-row>
+            <v-col
+              v-for="(category, i) in categories"
+              :key="`cat-${i}`"
+              :cols="getCategoryCols(category.text)"
+              class="pb-0"
+            >
+              <v-slide-x-reverse-transition>
+                <v-chip
+                  color="secondary"
+                  text-color="white"
+                  close
+                  class="d-inline-block text-center chip--category"
+                  @click:close="removeCategory(category)"
+                  >{{ category.text }}</v-chip
+                >
+              </v-slide-x-reverse-transition>
+            </v-col>
+          </v-row>
+        </div>
+      </v-fade-transition>
+
+      <!-- Suggested category list -->
+      <v-fade-transition>
+        <div v-if="categorySuggestionsActive.length" class="pt-4">
+          <h3 class="mb-3 secondary--text">Incase you need ideas</h3>
+
+          <v-item-group multiple>
+            <v-row>
+              <v-col
+                v-for="(category, i) in categorySuggestionsActive"
+                :key="`suggested-cat-${i}`"
+                class="pb-0"
+                cols="6"
+              >
+                <v-item>
+                  <v-chip
+                    text-color="white"
+                    class="d-inline-block text-center chip--category outline--dotted"
+                    @click="addSuggestedCategory(category)"
+                  >
+                    <v-card-subtitle>{{ category.text }}</v-card-subtitle>
+                  </v-chip>
+                </v-item>
+              </v-col>
+            </v-row>
+          </v-item-group>
+        </div>
+      </v-fade-transition>
+    </section>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'CategoriesForm',
+  props: {
+    onUpdated: {
+      type: Function,
+      default: (categories) => {},
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "Let's get organized",
+    },
+  },
+  data() {
+    return {
+      categoryToAdd: '',
+      categories: [],
+      categorySuggestions: [
+        { text: 'Ideas', isSuggestion: true, isActive: true },
+        { text: 'Business', isSuggestion: true, isActive: true },
+        { text: 'Gratitude', isSuggestion: true, isActive: true },
+        { text: 'Spirituality', isSuggestion: true, isActive: true },
+      ],
+      MAX_CATEGORIES: 8,
+    }
+  },
+  computed: {
+    canAddCategory() {
+      const canAdd =
+        !_.isEmpty(this.categoryToAdd) && this.categoryToAdd.length >= 3
+
+      return canAdd
+    },
+    categorySuggestionsActive() {
+      const active = this.categorySuggestions.filter(
+        (suggestion) => suggestion.isActive
+      )
+
+      return active
+    },
+    categoriesToSubmit() {
+      const categories = this.categories.map((category) => category.text)
+
+      return categories
+    },
+    categoryLimitReached() {
+      const limitReached = this.categoriesToSubmit.length >= this.MAX_CATEGORIES
+
+      return limitReached
+    },
+  },
+  watch: {
+    categoryToAdd(value) {
+      value = value || ''
+      const valueFormatted = value.replace(/\s/g, ' ').toLowerCase()
+
+      this.categoryToAdd = _.upperFirst(valueFormatted)
+    },
+    categoriesToSubmit(categories) {
+      // Pass this information to on updated
+      this.onUpdated(categories)
+    },
+  },
+  methods: {
+    /** Get the number of columns a category should occupy based on its length */
+    getCategoryCols(categoryText) {
+      const cols = categoryText.length > 12 ? '12' : '6'
+
+      return cols
+    },
+
+    categoryIsDuplicate() {
+      // TODO: Add implementation
+    },
+    addCategory() {
+      const category = {
+        text: this.categoryToAdd,
+        isSuggestion: false,
+      }
+
+      this.categories = [...this.categories, category]
+
+      // Clear the category input field
+      this.categoryToAdd = ''
+    },
+    //
+    addSuggestedCategory(category) {
+      this.categories = [...this.categories, category]
+
+      this.categorySuggestions = this.categorySuggestions.map(
+        (categorySuggested) => {
+          if (
+            categorySuggested.text.toLowerCase() === category.text.toLowerCase()
+          ) {
+            categorySuggested.isActive = false
+          }
+
+          return categorySuggested
+        }
+      )
+    },
+    removeCategory(categoryToRemove) {
+      // Remove from normal categories
+      this.categories = this.categories.filter(
+        (category) => category !== categoryToRemove
+      )
+
+      // Reactivate the category suggestion if it was a suggestion
+      if (categoryToRemove.isSuggestion) {
+        this.categorySuggestions = this.categorySuggestions.map(
+          (suggestion) => {
+            if (
+              suggestion.text.toLowerCase() ===
+              categoryToRemove.text.toLowerCase()
+            ) {
+              suggestion.isActive = true
+            }
+
+            return suggestion
+          }
+        )
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.chip--category {
+  width: 100%;
+}
+.outline--dotted {
+  border: 1px dotted #8d8d8d;
+}
+</style>
