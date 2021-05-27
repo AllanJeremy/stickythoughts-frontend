7e3ee52fabbf63504a79d16fbeef2dbe7ad3a645
Allan Jeremy <aj.dev254@gmail.com>
2021-05-27 15:36:51 +0300

    [Refactoring] Only storing the user's uid as part of the customer types data Realized there's no need to collect more information than we need for this analysis. The onyl reason we have the uid here is to figure out changes in user behaviour in the long term without necessarily caring about which specific user it was

--- apiRequests/marketInsight/addCustomerTypes.js
+++ apiRequests/marketInsight/addCustomerTypes.js
@@ -2,12 +2,12 @@ import { postRequest } from '../../helpers/api'
 
 /** Add new customer types eg. Student, Entrepreneur - Attached to the user
  * @description This will ignore creation if the user already exists and data returned will be null
- * @param {Object} userData An object representing the data of the user this customer type belongs to
+ * @param {UUID} uid A string representing the currently logged in user's uid
  * @param {Object} customerTypesData An object representing the data of the customer type to be created
  * @return {Promise<ApiResponse>} A promise that resolves to an ApiResponse object
  */
-const addCustomerTypes = (userData, customerTypesData) => {
-  return postRequest(`/insight/customer-types/${userData.uid}`, {
+const addCustomerTypes = (uid, customerTypesData) => {
+  return postRequest(`/insight/customer-types/${uid}`, {
     data: customerTypesData,
   })
 }
