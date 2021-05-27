b99ad1e0dddfc0c3b057be03301c309f36986e1f
Allan Jeremy <aj.dev254@gmail.com>
2021-05-27 13:23:21 +0300

    [Data] Added customer type suggestions data file

---
+++ data/customerTypeSuggestions.js
@@ -0,0 +1,17 @@
+/** An object representing the default customer type */
+const defaultCustomerTypes = [{ text: 'Other', value: 'other' }]
+
+/** An array of objects representing customer types */
+const customerTypeSuggestions = [
+  { text: 'Student', value: 'student' },
+  { text: 'Creative', value: 'fitness' },
+  { text: 'Entrepreneur', value: 'entrepreneur' },
+  { text: 'Journaler', value: 'journaling' },
+  { text: 'Musician', value: 'musician' },
+  { text: 'Engineer', value: 'engineer' },
+  { text: 'Spiritual', value: 'spiritual' },
+  { text: 'Other', value: 'other' },
+]
+
+//* EXPORTS
+export { defaultCustomerTypes, customerTypeSuggestions }
