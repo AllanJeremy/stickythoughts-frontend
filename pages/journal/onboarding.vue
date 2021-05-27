3b1174386188d1855eb2d3117c044a9fce281d62
Allan Jeremy <aj.dev254@gmail.com>
2021-05-27 15:55:53 +0300

    [Feature] Onboarding now works E2E

--- pages/journal/onboarding.vue
+++ pages/journal/onboarding.vue
@@ -2,7 +2,7 @@
   <v-card
     class="py-4"
     :color="cardBackgroundColor"
-    height="85vh"
+    height="100%"
     max-height="35rem"
     width="90vw"
     max-width="25rem"
@@ -16,10 +16,11 @@
           step="1"
           :complete="currentStep > 1"
         ></v-stepper-step>
-
         <v-divider></v-divider>
+
         <v-stepper-step
           color="secondary"
+          complete-icon="mdi-check"
           step="2"
           :complete="currentStep > 2"
         ></v-stepper-step>
@@ -30,12 +31,37 @@
           step="3"
           :complete="currentStep > 3"
         ></v-stepper-step>
+
+        <v-divider></v-divider>
+        <v-stepper-step
+          color="secondary"
+          step="4"
+          :complete="currentStep > 4"
+        ></v-stepper-step>
       </v-stepper-header>
 
       <!-- Steps -->
       <v-stepper-items>
-        <!-- Step 1 - Welcome & Name entry -->
+        <!-- Step 1 - Customer feedback -->
         <v-stepper-content step="1">
+          <CustomerTypeForm :on-updated="updateCustomerTypesData">
+            <div class="mt-6 d-flex">
+              <v-spacer></v-spacer>
+              <v-btn
+                class="white--text"
+                color="teal"
+                depressed
+                :disabled="!btnSubmitCustomerTypesEnabled"
+                :loading="btnSubmitCustomerTypesLoading"
+                @click="submitCustomerTypes"
+                >Next
+              </v-btn>
+            </div>
+          </CustomerTypeForm>
+        </v-stepper-content>
+
+        <!-- Step 2 - Welcome & Name entry -->
+        <v-stepper-content step="2">
           <EmptyContainer image-url="/celebrate-balloon.svg">
             <h1 class="text-2x mb-1">Sweet! You're in.</h1>
             <p class="font-weight-light">Let us help you set things up.</p>
@@ -54,7 +80,7 @@
             ></v-text-field>
 
             <div class="d-flex flex-row justify-space-between">
-              <v-btn color="secondary" outlined @click="goToStep(2)"
+              <v-btn color="secondary" outlined @click="goToStep(3)"
                 >Skip</v-btn
               >
               <v-btn
@@ -69,12 +95,12 @@
           </EmptyContainer>
         </v-stepper-content>
 
-        <!-- Step 2 - Choose categories -->
-        <v-stepper-content step="2">
+        <!-- Step 3 - Choose categories -->
+        <v-stepper-content step="3">
           <CategoriesForm :on-updated="updateCategoriesData">
             <!-- Card actions - proceed to customization -->
             <div class="pt-8 d-flex">
-              <v-btn color="secondary" outlined @click="goToStep(1)"
+              <v-btn color="secondary" outlined @click="goToStep(2)"
                 >Back</v-btn
               >
               <v-spacer></v-spacer>
@@ -90,15 +116,15 @@
           </CategoriesForm>
         </v-stepper-content>
 
-        <!-- Step 3 - Customize UI -->
-        <v-stepper-content step="3">
+        <!-- Step 4 - Customize UI -->
+        <v-stepper-content step="4">
           <CustomizationForm :on-updated="updateCustomization">
             <div class="pt-8 d-flex">
               <v-btn
                 color="secondary"
                 outlined
                 :disabled="!btnCompleteOnboardingEnabled"
-                @click="goToStep(2)"
+                @click="goToStep(3)"
               >
                 Back
               </v-btn>
@@ -126,26 +152,40 @@ import { mapState } from 'vuex'
 
 // Data
 import { defaultColor } from '@/data/colorSuggestions'
+import { defaultErrorMessage } from '@/data/messages/feedback'
 
 // Api requests
-import { userApi } from '@/apiRequests'
+import { marketInsightApi, userApi } from '@/apiRequests'
 
 // Components
 import CategoriesForm from '@/components/forms/CategoriesForm.vue'
+import CustomerTypeForm from '@/components/forms/CustomerTypeForm.vue'
 import CustomizationForm from '@/components/forms/CustomizationForm.vue'
 import EmptyContainer from '@/components/EmptyContainer.vue'
 
+// Mixins
+import { NavMixin } from '@/mixins'
+
 export default {
-  components: { CategoriesForm, CustomizationForm, EmptyContainer },
+  components: {
+    CategoriesForm,
+    CustomerTypeForm,
+    CustomizationForm,
+    EmptyContainer,
+  },
+  mixins: [NavMixin],
   layout: 'onboarding',
   data() {
     return {
-      btnCompleteOnboardingEnabled: true, // Button is enabled by default
+      // 👇🏾 Added in order of appearance/requirement, not alphabetical
+      btnSubmitCustomerTypesLoading: false,
       btnCompleteOnboardingLoading: false,
+
       fullName: '',
       categoriesData: [],
-      currentStep: 3, // ? First step - "no 💩 Sherlock!""
+      currentStep: 1, // ? First step - "no 💩 Sherlock!""
       userUpdateData: {},
+      customerTypesData: null,
       customizationData: null,
       cardBackgroundColor: defaultColor.backgroundColor, // Reference `data/colorSuggestions.js` to understand object strucutre
     }
@@ -155,6 +195,20 @@ export default {
       userLoading: 'isLoading',
       userData: 'data',
     }),
+    btnCompleteOnboardingEnabled() {
+      const isEnabled = !this.btnCompleteOnboardingLoading
+
+      return isEnabled
+    },
+
+    btnSubmitCustomerTypesEnabled() {
+      // Only enable if the button is not loading and at least one customer type has been selected
+      const isEnabled =
+        !this.btnSubmitCustomerTypesLoading &&
+        !_.isEmpty(this.customerTypesData)
+
+      return isEnabled
+    },
   },
   watch: {
     fullName(name) {
@@ -178,9 +232,17 @@ export default {
     },
   },
   methods: {
+    // Categories update
     updateCategoriesData(categories) {
       this.categoriesData = categories
     },
+
+    // Customer types update
+    updateCustomerTypesData(customerTypes) {
+      this.customerTypesData = customerTypes
+    },
+
+    // Customization update
     updateCustomization(customization) {
       this.cardBackgroundColor = customization.color.background
       this.customizationData = customization
@@ -191,13 +253,37 @@ export default {
     },
 
     //* SUBMIT INFO FUNCTIONS - ACTIONS THAT MOVE TO THE NEXT STEP WHILE SAVING USER INPUT
+    submitCustomerTypes() {
+      // Set loading status
+      this.btnSubmitCustomerTypesLoading = true
+
+      // API request
+      marketInsightApi
+        .addCustomerTypes(this.userData.uid, this.customerTypesData)
+        .then(() => {
+          // TODO: Migrate this message to feedback messages data file
+          this.$toast.success('Thanks! This helps us improve for you.')
+
+          // Move to the next step in the onboarding process if successful
+          this.goToStep(2)
+        })
+        .catch(() => {
+          this.$toast.error(defaultErrorMessage)
+        })
+        .finally(() => {
+          // Wait 1 second before stopping the loading ~ gives time for redirects to occur without interruption
+          setTimeout(() => {
+            this.btnSubmitCustomerTypesLoading = false
+          }, 1000)
+        })
+    },
     submitName() {
       this.userUpdateData.name = this.fullName
-      this.goToStep(2)
+      this.goToStep(3)
     },
     submitCategories() {
       this.userUpdateData.categories = this.categoriesData
-      this.goToStep(3)
+      this.goToStep(4)
     },
     submitCustomizations() {
       this.userUpdateData.customization = this.customizationData
@@ -207,33 +293,28 @@ export default {
       this.submitCustomizations()
 
       this.btnCompleteOnboardingLoading = true
-      this.btnCompleteOnboardingEnabled = false
 
       // Update the user
       userApi
         .updateUser(this.userData.uid, this.userUpdateData)
         .then(() => {
+          // TODO: Migrate this message to feedback messages data file
           this.$toast.success(`That's it! Welcome onboard, ${this.fullName}`)
 
           this.goTo('/journal/record')
         })
-        .catch(() => {
-          this.$toast.error(
-            'Aww, shucks! Looks like we messed up. Please try again :(. Sorry.'
-          )
+        .catch((error) => {
+          this.$toast.error(defaultErrorMessage)
 
-          // Incase something went wrong, we re-enable the complete onboarding button so that they can try again
-          this.btnCompleteOnboardingEnabled = true
+          // eslint-disable-next-line no-console
+          console.error('Error details:', error)
         })
         .finally(() => {
-          this.btnCompleteOnboardingLoading = false
+          // Wait 1 second before stopping the loading ~ gives time for redirects to occur without interruption
+          setTimeout(() => {
+            this.btnCompleteOnboardingLoading = false
+          }, 1000)
         })
-
-      // Automatically re-enable the complete setup button after 3.5 seconds if it hasn't yet been re-enabled
-      setTimeout(() => {
-        this.btnCompleteOnboardingLoading = false
-        this.btnCompleteOnboardingEnabled = true
-      }, 3500)
     },
   },
 }
