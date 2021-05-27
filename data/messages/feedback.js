83c15c132bfd0b421e832ed08abf9f366e5cb586
Allan Jeremy <aj.dev254@gmail.com>
2021-05-27 13:19:10 +0300

    [Refactoring] Moved default error message to its own data file We'll be using this to  localize later

---
+++ data/messages/feedback.js
@@ -0,0 +1,6 @@
+// TODO: Consider adding messages in different languages. eg. `{en: '', es: ''}`
+const defaultErrorMessage =
+  'Aww, shucks! Looks like we messed up. Please try again. Sorry 😞.'
+
+//* EXPORTS
+export { defaultErrorMessage }
