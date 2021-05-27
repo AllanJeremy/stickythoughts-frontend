7223ec610d549b29c1e40de72fe7c4b830345efa
Allan Jeremy <aj.dev254@gmail.com>
2021-05-27 13:28:55 +0300

    [Lang] Added extra emoji to default error message

--- data/messages/feedback.js
+++ data/messages/feedback.js
@@ -1,6 +1,6 @@
 // TODO: Consider adding messages in different languages. eg. `{en: '', es: ''}`
-const defaultErrorMessage =
-  'Aww, shucks! Looks like we messed up. Please try again. Sorry 😞.'
+const defaultErrorMessage = `Aww, shucks 😩! Looks like we messed up. 
+  Please try again. Sorry 😞.`
 
 //* EXPORTS
 export { defaultErrorMessage }
