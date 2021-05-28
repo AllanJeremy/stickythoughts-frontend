fdb6874e07aefde9370dc107f6693e1898caf407
Allan Jeremy <aj.dev254@gmail.com>
2021-05-28 11:06:11 +0300

    [Enhancement] Added loggedInUser as an argument to the createJournal API request abstraction function Following the update to the create journal 'schema', creating a journal now requires us to pass in the currently logged in user's data
    This is used to map a user to a journal and is a critical component to the aforementioned functionality.

--- apiRequests/journal/createJournal.js
+++ apiRequests/journal/createJournal.js
@@ -1,11 +1,17 @@
 import { postRequest } from '../../helpers/api'
 
 /** Create a journal entry in the database
+ * @param {Object} loggedInUser The currently logged in user object
  * @param {Object} journalData The data to be entered in the database
  * @return {Promise<ApiResponse>} A promise the resolves into an API response object
  */
-const createJournal = (journalData) => {
+const createJournal = (loggedInUser, journalData) => {
   const url = '/journal'
+
+  // Embed user data into the request
+  journalData.uid = loggedInUser.uid
+  journalData.userFullName = loggedInUser.name
+
   const requestData = {
     data: journalData,
   }
