34c473c39ec6c5445e3997c520b509cbe036fb83
Allan Jeremy <aj.dev254@gmail.com>
2021-05-28 13:00:42 +0300

    [Feature] Journal creation via upload works Uploaded journals are saved in the database
    Events are fired off when the upload starts and when it ends

--- pages/journal/record.vue
+++ pages/journal/record.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div style="max-width: 40rem">
     <v-tabs
       class="mb-8"
       color="teal"
       slider-color="teal"
-      background-color="#ffefbb"
+      :background-color="uiCustomization.color.background"
       fixed-tabs
     >
       <v-tab class="transparent">Record</v-tab>
@@ -17,8 +17,7 @@
         <v-select
           v-model="selectedCategory"
           class="mt-2"
-          :items="categories"
-          :loading="categoriesLoading"
+          :items="userCategoriesSorted"
           solo
           color="teal"
           item-color="teal"
@@ -29,6 +28,7 @@
         <label class="font-weight-bold" for="recordCategory">Description</label>
         <v-textarea
           id="recordDescription"
+          v-model="description"
           class="mt-2"
           solo
           placeholder="Something to help you remember what you are talking about. You can leave this empty."
@@ -41,20 +41,34 @@
 </template>
 
 <script>
+import _ from 'lodash'
+import { mapState } from 'vuex'
+
+// API handlers
+import { journalApi } from '@/apiRequests'
+
+// Language
+import { defaultErrorMessage } from '@/data/messages/feedback'
+
+// Components
 import AudioRecorder from '@/components/audio/AudioRecorder.vue'
+
+// Mixins
+import { UploadMixin } from '@/mixins'
 
 export default {
   components: { AudioRecorder },
+  mixins: [UploadMixin],
   layout: 'app',
   data() {
     return {
       selectedCategory: { text: 'Ideas', value: '1' },
-      categoriesLoading: false,
-      categories: [
-        { text: 'Ideas', value: '1' },
-        { text: 'Business', value: '2' },
-        { text: 'Meditation', value: '3' },
-      ],
+      categoriesLoading: true,
+      categories: [],
+      description: '',
+      uploadProgress: 0,
+      currentUploadTask: null,
+      journalData: {},
     }
   },
   head() {
@@ -62,12 +76,142 @@ export default {
       title: 'Record',
     }
   },
+  computed: {
+    ...mapState('user', { uiCustomization: 'customization', userData: 'data' }),
+    userCategoriesSorted() {
+      // Make a copy of categories since sort() mutates the array it is called on and we can't mutate store values outside the store.
+      // We probably don't want to anyway
+      const categories =
+        JSON.parse(JSON.stringify(this.userData.categories)) || []
+      const sortedCategories = categories.sort()
+
+      //! Undocumented internal modification of external variable - consider alternative
+      // If categories were found - select the first one among the sorted ones
+
+      return sortedCategories
+    },
+  },
+  watch: {
+    currentUploadTask(uploadTask) {
+      if (_.isEmpty(uploadTask)) return
+
+      // Upload state changed
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          this.uploadProgress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+
+          // Data to broadcast to other pages/components
+          const uploadData = this.getUploadData(
+            this.uploadTask,
+            this.uploadProgress
+          )
+
+          this.$nuxt.$emit('uploading', uploadData)
+        },
+        (error) => {
+          this.$toast.error('Oops! Something went wrong with the upload')
+
+          // eslint-disable-next-line no-console
+          console.error('Error details: ', error)
+        },
+        () => {
+          // Emit upload complete event
+          this.$nuxt.$emit('uploadComplete')
+
+          // Upload is done ~ get the url to the uploaded asset
+          this.currentUploadTask.snapshot.ref
+            .getDownloadURL()
+            .then((downloadUrl) => {
+              //
+              this.journalData.assetLink = downloadUrl
+
+              journalApi
+                .createJournal(this.userData, this.journalData)
+                .then(() => {
+                  // Provide the user with feedback
+                  this.$toast.success('Journal entry upload successful')
+
+                  // Empty the description ~ it has been saved in the cloud
+                  this.description = ''
+                })
+                .catch(() => {
+                  this.$toast.error(defaultErrorMessage)
+                })
+                .finally(() => {
+                  // Reset the current upload task to prevent duplicates
+                  this.currentUploadTask = null
+                })
+            })
+        }
+      )
+    },
+  },
   created() {
+    // Handle a completed recording
     this.$nuxt.$on('recordingComplete', (trackDetails) => {
-      trackDetails.title = `Something in "${this.selectedCategory.text}"`
+      trackDetails.title = `Something in "${this.selectedCategory}"`
+
+      const metadata = {
+        title: `Something in ${trackDetails.title}`,
+        artist: this.userData.name,
+        duration: trackDetails.duration,
+        album: `${this.userData.name} - ${this.selectedCategory}`,
+        year: new Date().getFullYear(),
+        comment: this.description,
+      }
 
       this.$nuxt.$emit('loadTrack', trackDetails)
+
+      // Set the journal data
+      this.journalData = this.getJournalData(trackDetails)
+
+      // Start uploading the file automatically and store the upload task
+      this.currentUploadTask = this.uploadAudio(
+        trackDetails.file,
+        this.generateFileName(trackDetails.extension),
+        metadata
+      )
     })
+  },
+  mounted() {
+    // Pre-select the first category found if categories were found ~ which they usually will be (this is just some defensive programming)
+    if (this.userCategoriesSorted.length) {
+      this.selectedCategory = this.userCategoriesSorted[0]
+    }
+  },
+  methods: {
+    addUploadToQueue(uploadTask, extraData) {
+      // TODO: Add this to store
+    },
+    generateFileName(extension) {
+      const dateMs = new Date().getTime()
+      const randomSeed = _.random(1000)
+
+      const fileName = `Sticky-${_.upperFirst(
+        this.selectedCategory
+      )}-${dateMs}-${randomSeed}.${extension}`
+
+      return fileName
+    },
+
+    /** A getter that returns an object containing journal data to be added to the database in the appropriate format */
+    getJournalData(trackDetails) {
+      const journalData = {
+        duration: trackDetails.duration,
+        size: trackDetails.file.size,
+        description: this.description,
+        category: this.selectedCategory,
+      }
+
+      return journalData
+    },
+
+    uploadAudio(file, fileName, metadata) {
+      const fullFilePath = `audio/${fileName}`
+      return this.uploadFile(file, fullFilePath, metadata)
+    },
   },
 }
 </script>
