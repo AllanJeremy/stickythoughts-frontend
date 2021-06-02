bdd4669f4cd2d99e079a697e7ba21a6f7817c44b
Allan Jeremy <aj.dev254@gmail.com>
2021-06-02 15:08:33 +0300

    [Analytics Feature] Added hotjar mixin Also created an analytics directory in mixins to hold analytics related mixins

---
+++ mixins/Analytics/index.js
@@ -0,0 +1,8 @@
+/* 
+    This file is simply meant to be an aggregator for all mixins
+    This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
+*/
+import HotjarMixin from './HotjarMixin'
+
+// * EXPORTS
+export default { HotjarMixin }
--- mixins/index.js
+++ mixins/index.js
@@ -3,10 +3,18 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 import AudioPlayerMixin from './AudioPlayerMixin'
+import Analytics from './Analytics'
 import FormatMixin from './FormatMixin'
 import JournalMixin from './JournalMixin'
 import NavMixin from './NavMixin'
 import UploadMixin from './UploadMixin'
 
 // * EXPORTS
-export { AudioPlayerMixin, FormatMixin, JournalMixin, NavMixin, UploadMixin }
+export {
+  Analytics,
+  AudioPlayerMixin,
+  FormatMixin,
+  JournalMixin,
+  NavMixin,
+  UploadMixin,
+}
