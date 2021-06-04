6e0cd00da2a0b4854b47c460fe5b9c987fbdfe6e
Allan Jeremy <aj.dev254@gmail.com>
2021-06-04 21:01:32 +0300

    [Enhancement] Cleaned up base title for all pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - StickyThoughts',
-    title: 'Remember Everything - StickyThoughts',
+    title: 'Remember Everything',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
