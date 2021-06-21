9c917cbb728334eaee2c2719058929734b0bb26d
Allan Jeremy <aj.dev254@gmail.com>
2021-06-21 19:44:09 +0300

    [Documentation] Added documentation to the add recurring payment api request function

--- apiRequests/payment/addRecurringPayment.js
+++ apiRequests/payment/addRecurringPayment.js
@@ -1,5 +1,11 @@
 import { postRequest } from '../../helpers/api'
 
+/** Add a recurring payment - otherwise known as a subscription payment
+ * @param {Object} user The paying user's object (MUST contain `uid`, `name` & `email`)
+ * @param {String} paymentPlanName A string representing the name of the payment plan in the database
+ * @param {String} redirectUrl The url to redirect to on successful payment
+ * @return {Promise<PaymentRequestResponse>} The payment request response of this payment
+ */
 const addRecurringPayment = (loggedInUser, paymentPlan, redirectUrl) => {
   const { name, email, uid } = loggedInUser
 
