71a6ef82109c1990b1303543b3a1e26e43273df4
Allan Jeremy <aj.dev254@gmail.com>
2021-06-23 14:31:01 +0300

    [Enhancement] Added Auth mixin to overall mixin aggregator

--- mixins/index.js
+++ mixins/index.js
@@ -3,6 +3,7 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 import AudioPlayerMixin from './AudioPlayerMixin'
+import AuthMixin from './AuthMixin'
 import Analytics from './Analytics'
 import FormatMixin from './FormatMixin'
 import JournalMixin from './JournalMixin'
@@ -13,6 +14,7 @@ import UploadMixin from './UploadMixin'
 // * EXPORTS
 export {
   Analytics,
+  AuthMixin,
   AudioPlayerMixin,
   FormatMixin,
   JournalMixin,
