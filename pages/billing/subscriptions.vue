18284378248ac6ccad0056a6acd953661c63a2e9
Allan Jeremy <aj.dev254@gmail.com>
2021-06-28 20:10:46 +0300

    [Feature] Added subscriptions page

---
+++ pages/billing/subscriptions.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <SubscriptionForm />
+  </section>
+</template>
+
+<script>
+import SubscriptionForm from '@/components/subscriptions/SubscriptionForm.vue'
+
+export default {
+  components: { SubscriptionForm },
+  layout: 'site',
+  data() {
+    return {
+      // TODO: Add implementation
+    }
+  },
+  head() {
+    return {
+      title: 'Subscription',
+      description:
+        'Subscribe to StickyThoughts to start preserving your thoughts today.',
+    }
+  },
+}
+</script>
