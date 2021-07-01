86cc4bc0d867319c4af038d405048a0f45f1cb5a
Allan Jeremy <aj.dev254@gmail.com>
2021-07-01 10:29:01 +0300

    [Enhancement] Added mobile navigation for the site

--- layouts/site.vue
+++ layouts/site.vue
@@ -1,5 +1,56 @@
 <template>
   <v-app>
+    <!-- Navigation drawer -->
+    <v-navigation-drawer v-model="navIsOpen" app style="z-index: 999">
+      <v-list-item class="d-flex">
+        <v-list-item-content>
+          <nuxt-link to="/">
+            <v-img
+              src="/logo-horizontal-light-bg.svg"
+              alt="StickyThoughts logo"
+              width="14rem"
+          /></nuxt-link>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item
+          v-for="(item, i) in navItems"
+          :key="`lg-nav-${i}`"
+          replace
+          :to="item.external ? undefined : item.link"
+          :href="item.external ? item.link : undefined"
+          :target="item.external ? '__blank' : undefined"
+        >
+          <v-list-item-content>
+            <v-list-item-title
+              >{{ item.title.toUpperCase() }}
+              <v-icon v-if="item.external" small
+                >mdi-open-in-new</v-icon
+              ></v-list-item-title
+            >
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>
+            <!-- Login/Signup -->
+            <v-btn
+              class="font-weight-bold"
+              outlined
+              color="teal"
+              link
+              to="/auth/login"
+              >Login / Signup</v-btn
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar flat max-height="4rem">
       <nuxt-link to="/">
         <v-img
@@ -7,19 +58,16 @@
           alt="StickyThoughts logo"
           width="14rem"
       /></nuxt-link>
-      <nuxt-link to="/"> </nuxt-link>
       <v-spacer></v-spacer>
 
       <ul class="nav d-none d-md-inline-block">
-        <li>
-          <nuxt-link to="/why">Why StickyThoughts</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/pricing">Plans</nuxt-link>
-        </li>
-        <li>
-          <a target="_blank" href="https://blog.stickythoughts.app?source=app"
-            >Blog <v-icon small>mdi-open-in-new</v-icon></a
+        <li v-for="(item, i) in navItems" :key="`lg-nav-${i}`">
+          <nuxt-link v-if="!item.external" :to="item.link">{{
+            item.title
+          }}</nuxt-link>
+
+          <a v-if="item.external" target="_blank" :href="item.link"
+            >{{ item.title }} <v-icon small>mdi-open-in-new</v-icon></a
           >
         </li>
       </ul>
@@ -28,7 +76,7 @@
 
       <!-- Login/Signup -->
       <v-btn
-        class="font-weight-bold d-none d-sm-flex"
+        class="font-weight-bold d-none d-md-flex"
         outlined
         color="teal"
         link
@@ -36,7 +84,10 @@
         >Login / Signup</v-btn
       >
 
-      <!-- Navigation drawer -->
+      <v-app-bar-nav-icon
+        class="d-md-none"
+        @click.stop="navIsOpen = !navIsOpen"
+      ></v-app-bar-nav-icon>
     </v-toolbar>
     <v-main>
       <v-container fluid>
@@ -64,6 +115,21 @@ export default {
     return {
       fixed: false,
       navIsOpen: false,
+      navItems: [
+        {
+          title: 'Why StickyThoughts',
+          link: '/why',
+        },
+        {
+          title: 'Plans',
+          link: '/pricing',
+        },
+        {
+          title: 'Blog',
+          link: 'https://blog.stickythoughts.app?utm-source=app',
+          external: true,
+        },
+      ],
     }
   },
 }
