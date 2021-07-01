e388fa480cd3743ad3b6c2762e80ac65f65f1027
Allan Jeremy <aj.dev254@gmail.com>
2021-07-01 16:27:34 +0300

    [Feature] Added basic why page

--- pages/why.vue
+++ pages/why.vue
@@ -1,11 +1,77 @@
 <template>
   <section>
-    <h1>This is why</h1>
+    <v-parallax dark src="/hand-holding-golden-mic.jpg">
+      <div class="bg-full overlay"></div>
+      <v-row align="center" justify="center" style="z-index: 100">
+        <v-col class="text-center" cols="12">
+          <h1 class="text-h4 font-weight-bold mb-4">Why StickyThoughts?</h1>
+          <p class="font-weight-thin">
+            StickyThoughts lets you vocally record, organize and retrieve your
+            thoughts everywhere
+          </p>
+
+          <v-btn
+            class="white--text"
+            depressed
+            color="teal"
+            to="/auth/login"
+            x-large
+            >GET STARTED FOR FREE</v-btn
+          >
+          <p class="white--text">
+            <small><i>No credit card required</i></small>
+          </p>
+        </v-col>
+      </v-row>
+    </v-parallax>
+
+    <article
+      class="px-8 fill-height d-flex flex-column align-center justify-center text-center"
+    >
+      <h1 class="display-2 font-weight-bold mb-2">
+        We do less so you can do more
+      </h1>
+      <h2 class="display-1 grey--text mb-4">
+        Most note-taking apps do too much.
+      </h2>
+      <p class="font-weight-light">
+        StickyThoughts strives to be the simplest way to journal with your
+        voice.
+      </p>
+
+      <v-btn class="white--text" depressed color="teal" to="/auth/login" x-large
+        >GET STARTED FOR FREE</v-btn
+      >
+    </article>
   </section>
 </template>
 
 <script>
 export default {
   layout: 'site',
+  head() {
+    return {
+      title: 'Remember everything',
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.overlay {
+  background-color: black;
+  opacity: 0.64;
+  z-index: 0;
+}
+
+article {
+  min-height: 80vh;
+  width: 100%;
+  max-width: 50rem;
+  margin: auto;
+
+  p {
+    line-height: 180%;
+  }
+}
+</style>
