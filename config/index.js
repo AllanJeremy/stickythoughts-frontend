76babb07d55bbd0d4aed39ec57339d49ea099cae
Allan Jeremy <aj.dev254@gmail.com>
2021-07-03 16:07:37 +0300

    [Enhancement] Updated API url to a cleaner version mapped to our domain name

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://localhost:8080'
-const API_URL = 'https://stickythoughts-backend.herokuapp.com/'
+const API_URL = 'https://api.stickythoughts.app'
 
 // 5 Minutes
 const FREE_RECORDING_LIMIT_SECONDS = 300
