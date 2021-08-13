c8a4d8108154206f01662e574a4fa54e316984cd
Allan Jeremy <aj.dev254@gmail.com>
2021-08-13 09:03:38 +0300

    [Enhancement] Home page now redirects to app if a user is logged in

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,57 @@
 <template>
   <div>
-    <article
-      class="text-center d-flex flex-column justify-center align-center pa-3"
-      style="min-height: 50vh"
-    >
-      <h1 class="font-weight-bold" :style="`font-size: ${ctaTextFontSize}`">
-        Remember
-        <span :class="`${ctaShuffleText.color}--text`">{{
-          ctaShuffleText.text
-        }}</span>
-      </h1>
+    <!-- Hiding site contents if a user is logged in - TODO: Add a way to allow them to view the site -->
+    <Loading v-if="userLoading && !userLoggedIn" />
 
-      <h3 class="font-weight-light mt-2">
-        Finally, a simple organized way to privately record your thoughts on all
-        your devices
-      </h3>
-
-      <v-btn class="mt-6 white--text" color="teal" to="/auth/login" x-large
-        >GET STARTED FOR FREE</v-btn
+    <template v-else>
+      <article
+        class="text-center d-flex flex-column justify-center align-center pa-3"
+        style="min-height: 50vh"
       >
-      <p class="secondary--text">
-        <small><i>No credit card required</i></small>
-      </p>
-    </article>
+        <h1 class="font-weight-bold" :style="`font-size: ${ctaTextFontSize}`">
+          Remember
+          <span :class="`${ctaShuffleText.color}--text`">{{
+            ctaShuffleText.text
+          }}</span>
+        </h1>
 
-    <div class="d-flex flex-column align-center" style="height: 100vh">
-      <img
-        src="/mockup.png"
-        alt="StickyThoughts audio notes mockup"
-        width="800"
-        style="max-width: 100%"
-      />
-    </div>
+        <h3 class="font-weight-light mt-2">
+          Finally, a simple organized way to privately record your thoughts on
+          all your devices
+        </h3>
+
+        <v-btn class="mt-6 white--text" color="teal" to="/auth/login" x-large
+          >GET STARTED FOR FREE</v-btn
+        >
+        <p class="secondary--text">
+          <small><i>No credit card required</i></small>
+        </p>
+      </article>
+
+      <div class="d-flex flex-column align-center" style="height: 100vh">
+        <img
+          src="/mockup.png"
+          alt="StickyThoughts audio notes mockup"
+          width="800"
+          style="max-width: 100%"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+// Mixins
+import { NavMixin } from '@/mixins'
+
+// Components
+import Loading from '@/components/Loading.vue'
+
 export default {
+  components: { Loading },
+  mixins: [NavMixin],
   layout: 'site',
   data() {
     return {
@@ -50,6 +65,11 @@ export default {
     }
   },
   computed: {
+    ...mapState('user', {
+      userLoading: 'isLoading',
+      userLoggedIn: 'isLoggedIn',
+    }),
+
     ctaTextFontSize() {
       switch (this.$vuetify.breakpoint.name) {
         case 'xs':
@@ -58,6 +78,15 @@ export default {
 
         default:
           return '3rem'
+      }
+    },
+  },
+  watch: {
+    userLoading(isLoading) {
+      // When the user is done loading - redirect accordingly.
+      if (isLoading === false && this.userLoggedIn) {
+        this.$toast.info('Logged in. Redirecting to journal...')
+        this.goTo('/journal')
       }
     },
   },
